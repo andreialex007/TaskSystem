44f6b8decb735e9fbb6cdf84bd6bf728a5edfafb
Andrey <andreialexandrovich@gmail.com>
2017-12-25 00:14:45 +0300

    adding datatable

---
+++ TaskSystem/TaskSystem/src/components/common/DataTable.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        props: {
+            options: { type: Object }
+        },
+        data() {
+            return {
+                table: null
+            }
+        },
+        mounted() {
+            this.table = $(this.$el).find("table").dataTable(this.options || {});
+            this.table.on('draw', function () {
+                console.log("table-has-been-drawn");
+            });
+        }
+    }
+</script>
--- TaskSystem/TaskSystem/src/components/users/UsersPage.vue
+++ TaskSystem/TaskSystem/src/components/users/UsersPage.vue
@@ -3,17 +3,27 @@
         <div class="col-lg-12">
             <h1 class="mt-5">Users page</h1>
             <hr />
-            <table class="test-table table">
-                <thead>
-                    <tr>
-                        <th>Subscriber ID</th>
-                        <th>Install Location</th>
-                        <th>Subscriber Name</th>
-                        <th>some data</th>
-                    </tr>
-                </thead>
-                <tbody></tbody>
-            </table>
+            <data-table v-bind:options="tableOptions">
+                <div class="data-table-template" >
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                    <div class="edit-column-template data-table-template">
+                        <a title="edit" class="edit btn default btn-xs blue" href="javascript:;">
+                            <i class="fa fa-edit"></i>
+                        </a>
+                    </div>
+                </div>
+            </data-table>
 
 
         </div>
@@ -22,14 +32,80 @@
 
 
 <script>
-    import mainLayout from "./../layout/MainLayout.vue"
+    import mainLayout from "./../layout/MainLayout.vue";
+    import dataTable from "./../common/DataTable.vue"
 
     export default {
         components: {
-            mainLayout
+            mainLayout,
+            dataTable
+        },
+        data() {
+            return {
+                tableOptions: {
+                    columns: [
+                        { "data": "Id" },
+                        { "data": "FirstName" },
+                        { "data": "LastName" },
+                        { "data": "Email" },
+                        { "data": "Actions" }
+                    ],
+                    "aoColumnDefs": [
+                        {
+                            bSortable: false,
+                            sDefaultContent: $("#tickets-list-page-actions-column-template").html(),
+                            aTargets: [-1]
+                        }
+                    ],
+                    order: [
+                        [0, "asc"]
+                    ]
+                }
+            };
         },
         mounted() {
-            $(".test-table").dataTable();
+            /*
+             self.table = $('.tickets-list-page table').DataTable({
+                "processing": true,
+                "serverSide": true,
+                "ajax": {
+                    "url": "/Tickets/Search",
+                    "type": "POST",
+                    data: function (data) {
+                        data.isCurrent = $(".current-filter :checkbox").is(":checked");
+                    }
+                },
+                "columns": [
+                    { "data": "PKID" },
+                    { "data": "CustomerName" },
+                    { "data": "Status" },
+                    { "data": "Priority" },
+                    { "data": "AssignedTechnician" },
+                    { "data": "Actions" }
+                ],
+                aoColumnDefs: [
+                    {
+                        "render": function (data, type, row) {
+                            return "<span class='priority-color-mark' style='background-color: " + row.PriorityColor + "' ></span>" + data;
+                        },
+                        "targets": 3
+                    },
+                    {
+                        bSortable: false,
+                        sDefaultContent: $("#tickets-list-page-actions-column-template").html(),
+                        aTargets: [-1]
+                    }
+                ],
+                order: [
+                    [0, "asc"]
+                ]
+            });
+
+            */
+
+
+
+            //  $(".test-table").dataTable();
         }
     }
 </script>
