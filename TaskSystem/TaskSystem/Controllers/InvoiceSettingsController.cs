eb7a6e2df275cf4f64315f5a4238fb89cb9f50e4
Andrey <andreialexandrovich@gmail.com>
2018-01-19 23:21:34 +0300

    invoice settings controller added

---
+++ TaskSystem/TaskSystem/Controllers/InvoiceSettingsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TaskSystem.BL.Models;
+using TaskSystem.DL;
+using TaskSystem.DL.Entities.Invoices;
+using ControllerBase = TaskSystem.Controllers._Common.ControllerBase;
+
+namespace TaskSystem.Controllers
+{
+    public class InvoiceSettingsController : ControllerBase
+    {
+        public InvoiceSettingsController(IConfiguration configuration, AppDbContext appDbContext, IHostingEnvironment hostingEnvironment)
+            : base(configuration, appDbContext, hostingEnvironment)
+        {
+        }
+
+        [HttpPost]
+        [Route("InvoiceElementCategories")]
+        public IActionResult InvoiceElementCategories()
+        {
+            var items = Service.InvoiceElementCategory.All();
+            return Ok(items);
+        }
+
+        [HttpPost]
+        [Route("CommonInvoiceElements")]
+        public IActionResult CommonInvoiceElements()
+        {
+            var items = Service.CommonInvoiceElement.All();
+            return Ok(items);
+        }
+
+        [HttpPost]
+        [Route("Settings")]
+        public IActionResult Settings()
+        {
+            var elements = Service.CommonInvoiceElement.All();
+            var categories = Service.InvoiceElementCategory.All();
+            return Ok(new
+            {
+                elements,
+                categories
+            });
+        }
+
+        [HttpPost]
+        [Route("SaveCommonInvoiceElement")]
+        public IActionResult SaveInvoice([FromBody] CommonInvoiceElementItem item)
+        {
+            Service.CommonInvoiceElement.Save(item);
+            return Ok(item);
+        }
+
+        [HttpPost]
+        [Route("SaveInvoiceElementCategory")]
+        public IActionResult SaveInvoice([FromBody] InvoiceElementCategoryItem item)
+        {
+            Service.InvoiceElementCategory.Save(item);
+            return Ok(item);
+        }
+
+        [HttpPost]
+        [Route("DeleteCommonInvoiceElement/{id?}")]
+        public IActionResult DeleteCommonInvoiceElement(int id)
+        {
+            Service.CommonInvoiceElement.Delete(id);
+            return Ok();
+        }
+        [HttpPost]
+        [Route("DeleteInvoiceElementCategory/{id?}")]
+        public IActionResult DeleteInvoiceElementCategory(int id)
+        {
+            Service.InvoiceElementCategory.Delete(id);
+            return Ok();
+        }
+    }
+}
